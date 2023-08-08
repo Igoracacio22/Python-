68a075d4d8faea81af0c4f5b3771798788823156
Igor Alves <igor.alves@transfero.com>
2023-08-08 16:14:58 -0300

    primeiro commit

---
+++ Exercícios/ex04.py
@@ -0,0 +1,7 @@
+algo = input('Digite algo:')
+
+
+print(type(algo))
+print(f'É numérico ? {algo.isnumeric()}')
+print(f'É alfabético ? {algo.isalpha()}')
+print(f'É maiúsculo ? {algo.isupper()}')
---
+++ Treino-Python/ex02.py
@@ -0,0 +1,6 @@
+dia = input("Dia =")
+mes = input("Mês =")
+ano = input("Ano = ")
+
+print("Você nasceu no dia {} de {} de {}. Correto?".format(dia,mes,ano))
+
